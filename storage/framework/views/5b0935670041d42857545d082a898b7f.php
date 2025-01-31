6871e1287703eb7ac3399862517cfa8d851a4631
nethmitharushika7 <nethmitharushikaperera7@gmail.com>
2025-01-31 11:13:00 -0800

    Create 5b0935670041d42857545d082a898b7f.php

---
+++ storage/framework/views/5b0935670041d42857545d082a898b7f.php
@@ -0,0 +1,278 @@
+<?php $__env->startSection('content'); ?>
+        <!-- Page Title -->
+        <section class="page-title style-two" style="background-image: url(newFrontend/Clasifico/assets/images/background/page-title.jpg);">
+            <div class="auto-container">
+                <div class="mr-0 content-box centred">
+                    <div class="title">
+                        <h1>About Us</h1>
+                    </div>
+                    <ul class="clearfix bread-crumb">
+                        <li><a href="index.html">Home</a></li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- End Page Title -->
+
+
+        <!-- about-section -->
+        <section class="about-section">
+            <div class="auto-container">
+                <div class="clearfix row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 content-column">
+                        <div class="content_block_3">
+                            <div class="content-box">
+                                <div class="sec-title">
+                                    <span>About</span>
+                                    <h2>About Our Company</h2>
+                                </div>
+                                <div class="text">
+                                    <p>Yaka.lk is the largest growing market place in Sri Lanka. This is a 100 % Sri Lankan website which designed specially to suit Sri Lankans. If you want to buy or sell anything, you have arrived to the right destination.Yaka.lk has the broad selection of items so you can navigate through many categories such as Electronics, Vehicles, Property, jobs, Industrial, etc., also you can use search filters in order to make it quick in findings.You can create free account in yaka.lk and post your advertisement within no time and as soon as you publish, we will review it and allow to view in website. Also, you can choose add promotion packages for better results.</p>
+                                    <h3>What We Do</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 image-column">
+                        <div class="image_block_1">
+                            <div class="image-box">
+                                <div class="image-pattern">
+                                    <div class="pattern-1" style="background-image: url(newFrontend/Clasifico/assets/images/shape/shape-16.png);"></div>
+                                    <div class="pattern-2" style="background-image: url(newFrontend/Clasifico/assets/images/shape/shape-16.png);"></div>
+                                </div>
+                                <figure class="image"><img src="newFrontend/Clasifico/assets/images/resource/about-1.jpg" alt=""></figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- about-section end -->
+
+
+        <!-- process-section -->
+        <section class="process-section centred sec-pad">
+            <div class="pattern-layer" style="background-image: url(newFrontend/Clasifico/assets/images/shape/shape-17.png);"></div>
+            <div class="auto-container">
+                <div class="sec-title centred">
+                    <span>Process</span>
+                    <h2>How it Works</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing sed do eiusmod tempor <br />incididunt labore dolore magna aliqua enim.</p>
+                </div>
+                <div class="inner-content">
+                    <div class="clearfix row">
+                        <div class="col-lg-4 col-md-6 col-sm-12 process-block">
+                            <div class="process-block-one">
+                                <div class="inner-box">
+                                    <span class="count wow fadeInDown animated" data-wow-delay="00ms" data-wow-duration="1500ms">01</span>
+                                    <div class="text wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                                        <div class="icon-box"><i class="fas fa-user"></i></div>
+                                        <h3>Create Account</h3>
+                                        <p>Lorem ipsum dolor sit amet constur adipisicing sed do eiusmod tempor incididunt labore.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 process-block">
+                            <div class="process-block-one">
+                                <div class="inner-box">
+                                    <span class="count wow fadeInDown animated" data-wow-delay="300ms" data-wow-duration="1500ms">02</span>
+                                    <div class="text wow fadeInUp animated" data-wow-delay="300ms" data-wow-duration="1500ms">
+                                        <div class="icon-box"><i class="fas fa-glass-martini"></i></div>
+                                        <h3>Post Your Ads</h3>
+                                        <p>Lorem ipsum dolor sit amet constur adipisicing sed do eiusmod tempor incididunt labore.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-sm-12 process-block">
+                            <div class="process-block-one">
+                                <div class="inner-box">
+                                    <span class="count wow fadeInDown animated" data-wow-delay="600ms" data-wow-duration="1500ms">03</span>
+                                    <div class="text wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms">
+                                        <div class="icon-box"><i class="fas fa-dollar-sign"></i></div>
+                                        <h3>Sell Your Item</h3>
+                                        <p>Lorem ipsum dolor sit amet constur adipisicing sed do eiusmod tempor incididunt labore.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- process-section end -->
+
+
+        <!-- testimonial-section -->
+        <section class="testimonial-section bg-color-1 sec-pad">
+            <figure class="image-layer"><img src="assets/images/resource/testimonial-image-1.png" alt=""></figure>
+            <div class="anim-icon">
+                <div class="icon anim-icon-1 rotate-me" style="background-image: url(newFrontend/Clasifico/assets/images/icons/anim-icon-1.png);"></div>
+                <div class="icon anim-icon-2 rotate-me" style="background-image: url(newFrontend/Clasifico/assets/images/icons/anim-icon-2.png);"></div>
+                <div class="icon anim-icon-3 rotate-me" style="background-image: url(newFrontend/Clasifico/assets/images/icons/anim-icon-2.png);"></div>
+                <div class="icon anim-icon-4 rotate-me" style="background-image: url(newFrontend/Clasifico/assets/images/icons/anim-icon-1.png);"></div>
+            </div>
+            <div class="pattern-layer">
+                <div class="pattern-1" style="background-image: url(newFrontend/Clasifico/assets/images/shape/shape-4.png);"></div>
+                <div class="pattern-2" style="background-image: url(newFrontend/Clasifico/assets/images/shape/shape-5.png);"></div>
+                <div class="pattern-3" style="background-image: url(newFrontend/Clasifico/assets/images/shape/shape-6.png);"></div>
+            </div>
+            <div class="auto-container">
+                <div class="clearfix row">
+                    <div class="col-xl-6 col-lg-12 col-md-12 inner-column">
+                        <div class="inner-box">
+                            <div class="sec-title light">
+                                <span>Testimonials</span>
+                                <h2>What client’s say?</h2>
+                            </div>
+                            <div class="single-item-carousel owl-carousel owl-theme owl-dots-none nav-style-one">
+                                <div class="testimonial-content">
+                                    <div class="text">
+                                        <p>“ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim  minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip consequat aute ux irure dolor in reprehen.”</p>
+                                    </div>
+                                    <div class="author-box">
+                                        <figure class="author-thumb"><img src="newFrontend/Clasifico/assets/images/resource/testimonial-1.png" alt=""></figure>
+                                        <h3>Amelia Anna</h3>
+                                        <span class="designation">Senior Martketer</span>
+                                    </div>
+                                </div>
+                                <div class="testimonial-content">
+                                    <div class="text">
+                                        <p>“ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim  minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip consequat aute ux irure dolor in reprehen.”</p>
+                                    </div>
+                                    <div class="author-box">
+                                        <figure class="author-thumb"><img src="newFrontend/Clasifico/assets/images/resource/testimonial-1.png" alt=""></figure>
+                                        <h3>Amelia Anna</h3>
+                                        <span class="designation">Senior Martketer</span>
+                                    </div>
+                                </div>
+                                <div class="testimonial-content">
+                                    <div class="text">
+                                        <p>“ Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim  minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip consequat aute ux irure dolor in reprehen.”</p>
+                                    </div>
+                                    <div class="author-box">
+                                        <figure class="author-thumb"><img src="newFrontend/Clasifico/assets/images/resource/testimonial-1.png" alt=""></figure>
+                                        <h3>Amelia Anna</h3>
+                                        <span class="designation">Senior Martketer</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- testimonial-section end -->
+
+
+        <!-- pricing-section -->
+        <section class="pricing-section sec-pad">
+            <div class="auto-container">
+                <div class="sec-title centred">
+                    <span>Pricing Table</span>
+                    <h2>No Any Hidden Charge Select <br />Your Pricing Plan</h2>
+                </div>
+                <div class="clearfix row">
+                    <div class="col-lg-4 col-md-6 col-sm-12 pricing-block">
+                        <div class="pricing-block-one">
+                            <div class="pricing-table">
+                                <div class="teble-header">
+                                    <p>Basic Pack</p>
+                                    <h2>$30 <span>/ month</span></h2>
+                                </div>
+                                <div class="table-content">
+                                    <ul class="clearfix list">
+                                        <li>5+ Listings</li>
+                                        <li>Contact With Agent</li>
+                                        <li>Contact With Agent</li>
+                                        <li class="light">7×24 Fully Support</li>
+                                        <li class="light">50GB Space</li>
+                                    </ul>
+                                </div>
+                                <div class="table-footer">
+                                    <a href="index.html">Register Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 pricing-block">
+                        <div class="pricing-block-one active">
+                            <div class="pricing-table">
+                                <div class="teble-header">
+                                    <p>Standard Pack</p>
+                                    <h2>$40 <span>/ month</span></h2>
+                                </div>
+                                <div class="table-content">
+                                    <ul class="clearfix list">
+                                        <li>5+ Listings</li>
+                                        <li>Contact With Agent</li>
+                                        <li>Contact With Agent</li>
+                                        <li class="light">7×24 Fully Support</li>
+                                        <li class="light">50GB Space</li>
+                                    </ul>
+                                </div>
+                                <div class="table-footer">
+                                    <a href="index.html">Register Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-12 pricing-block">
+                        <div class="pricing-block-one">
+                            <div class="pricing-table">
+                                <div class="teble-header">
+                                    <p>Premium Pack</p>
+                                    <h2>$60 <span>/ month</span></h2>
+                                </div>
+                                <div class="table-content">
+                                    <ul class="clearfix list">
+                                        <li>5+ Listings</li>
+                                        <li>Contact With Agent</li>
+                                        <li>Contact With Agent</li>
+                                        <li class="light">7×24 Fully Support</li>
+                                        <li class="light">50GB Space</li>
+                                    </ul>
+                                </div>
+                                <div class="table-footer">
+                                    <a href="index.html">Register Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- pricing-section end -->
+
+
+        <!-- subscribe-section -->
+        <section class="subscribe-section">
+            <div class="pattern-layer" style="background-image: url(newFrontend/Clasifico/assets/images/shape/shape-9.png);"></div>
+            <div class="auto-container">
+                <div class="clearfix row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 text-column">
+                        <div class="text">
+                            <div class="icon-box"><i class="icon-25"></i></div>
+                            <h2>Subscribe to Newsletter</h2>
+                            <p>and receive new ads in inbox</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 form-column">
+                        <form action="contact.html" method="post" class="subscribe-form">
+                            <div class="form-group">
+                                <input type="email" name="email" placeholder="Inout your email address" required="">
+                                <button type="submit" class="theme-btn-one">Subscribe Now</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- subscribe-section end -->
+
+ <?php $__env->stopSection(); ?>       
+
+<?php echo $__env->make('newFrontend.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp1\htdocs\Yaka-Project\resources\views/newFrontend/about-us.blade.php ENDPATH**/ ?>
