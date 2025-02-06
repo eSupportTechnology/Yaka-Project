739917893f37f8f30d484c6035a12f1f5b832267
Kasuni Gayanthika <gayanthikakasuni3@gmail.com>
2025-02-06 16:23:00 +0530

    modify general links pages

--- resources/views/newFrontend/boosting_ads.blade.php
+++ resources/views/newFrontend/boosting_ads.blade.php
@@ -370,6 +370,21 @@
     </div>
 </section>
 
+<div class="container">
+    <div class="row">
+      <div class="pt-5 pb-5 col-md-12">
+        <h2 class="mt-5 mb-4 text-center">Boosting Ads</h2>
+        <p class="mb-4 text-center">Our boosting methods are powered by a fully sophisticated AI-generated algorithm, ensuring quicker results.</p>
+        <ul class="text-center boosting-list">
+          <li class="mb-2">1. Jump up ads</li>
+          <li class="mb-2">2. Top ads</li>
+          <li class="mb-2">3. Urgent ads</li>
+          <li class="mb-2">4. Super ad</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
     <!-- Steps Section -->
     <section class="steps">
         <h2>How to Boost Your Ad?</h2>
@@ -465,7 +480,6 @@
         <p>Get more views, more customers, and better sales.</p>
         <a href="#" class="btn">Get Started</a>
     </section>
-   
 
-    @endsection
+@endsection
 
