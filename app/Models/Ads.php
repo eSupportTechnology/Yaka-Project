dad3e9967a7cfc52af2c242b51ec811824216bda
nethmitharushika7 <nethmitharushikaperera7@gmail.com>
2025-02-07 09:13:58 -0800

    Update Ads.php

--- app/Models/Ads.php
+++ app/Models/Ads.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ads extends Model
 {
+    protected $table = 'ads';
     protected $fillable = [
         'title',
         'url',
