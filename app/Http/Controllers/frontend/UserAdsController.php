a0a58e82001d1d210a9e6c7b5ecac2201936b526
GTec <gayashancs7@gmail.com>
2025-05-14 13:35:08 +0530

    Fix issue

--- app/Http/Controllers/frontend/UserAdsController.php
+++ app/Http/Controllers/frontend/UserAdsController.php
@@ -213,7 +213,7 @@ class UserAdsController extends Controller
 
             // Step 3: Add watermark text
             $image->text('YAKA.LK', 10, 10, function ($font) {
-                $font->filename(public_path('fonts/arial.ttf')); // Note: filename() instead of file()
+                $font->filename(asset('public/fonts/arial.ttf'));
                 $font->size(24);
                 $font->color('#FF0000');
                 $font->align('left');
