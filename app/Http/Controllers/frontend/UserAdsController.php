8430fa092c429556f201e47dac878e2c00e2215d
GTec <gayashancs7@gmail.com>
2025-05-14 13:31:25 +0530

    Fix issue

--- app/Http/Controllers/frontend/UserAdsController.php
+++ app/Http/Controllers/frontend/UserAdsController.php
@@ -234,7 +234,7 @@ class UserAdsController extends Controller
                     if ($file->isValid()) {
                         $image = $manager->read($file->getPathname());
                         $image->text('YAKA.LK', 10, 10, function ($font) {
-                            $font->filename(public_path('fonts/arial.ttf'));
+                            $font->filename(asset('public/fonts/arial.ttf'));
                             $font->size(24);
                             $font->color('#FF0000');
                             $font->align('left');
