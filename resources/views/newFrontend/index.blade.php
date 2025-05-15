dafc7140c01d66aa291963e825c403b86fa6ef6c
ICT185 <pathumadubashana76@gmail.com>
2025-05-15 18:29:04 +0530

    image

--- resources/views/newFrontend/index.blade.php
+++ resources/views/newFrontend/index.blade.php
@@ -977,6 +977,11 @@
             startSlideshow(superBannerItems);
         });
     </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script>
+    new WOW().init();
+</script>
 
 
 @endsection
