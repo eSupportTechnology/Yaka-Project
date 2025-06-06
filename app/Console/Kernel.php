be44b45d572525f6dfa1158ccb47a7e35a6687ff
GTec <gayashancs7@gmail.com>
2025-06-06 19:26:52 +0530

    Add console

---
+++ app/Console/Kernel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        // Register your custom commands here
+        \App\Console\Commands\AdsRotateSuper::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('ads:rotate:super')->daily();
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
